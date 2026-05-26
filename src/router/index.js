d0f4e057c6bdd7902a59af1cc4ad0b6509ff84c2
Switchot <cswitchot3@gmail.com>
2026-05-25 20:44:27 -0400

    Ajout routerForHashmode

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import { createWebHistory, createRouter } from "vue-router";
-import Home from "@/App.vue"
+import Home from "@/components/Home.vue"
 import Map from "@/components/Map.vue"
 
 const routes = [
+	{
+		name: "Home",
+		path: "/",
+		component: Home
+	},
 	{
 		name: "map",
 		path: "/map",
@@ -11,8 +16,8 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes,
-});
+  history: createWebHashHistory(),
+  routes
+})
 
 export default router;
